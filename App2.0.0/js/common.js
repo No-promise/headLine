cad37f72bebb96a1fea8c2e842c3849de22a9d23
吴蓉蓉 <wrr@janesi.net>
2018-10-16 14:22:29 +0800

    2.2.0

---
+++ App2.0.0/js/common.js
@@ -0,0 +1,25 @@
+// var urlBase = 'http://yun1.janesi.net';
+var urlBase = 'http://yun.janesi.com';
+
+//正式
+var requestUrl = 'http://zq.janesi.com';  
+// 预发
+// var requestUrl = 'http://pre.zq.janesi.com'
+
+// 测试
+// var requestUrl = 'http://118.25.10.151:8090';
+// var requestUrl = 'http://10.10.11.41:8090';
+    var appId='10010';
+    var userId='1483';  //41
+    var osType='ANDORID';
+    var apiVersion = '2.1.0';
+
+var reqParams = {
+        // 'appId':'10010',
+        // // 'userId':'247322',
+        // 'userId':'389',
+        // // 'apiVersion':'2.1.0'
+}
+
+
+                    
